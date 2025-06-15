de4bc04a69f5296f9457a0f9be369f439a76a4d4
Tobyjcode <tobias-jes@hotmail.com>
2025-06-15 09:29:17 +0200

    #Level design

--- scenes/game.gd
+++ scenes/game.gd
@@ -13,21 +13,6 @@ func _ready():
 		start_door.connect("start_door_opened", Callable(player, "play_door_out"))
 		start_door.start_open_sequence()
 
-	# Make buttons more visible when focused
-	var focus_style = StyleBoxFlat.new()
-	focus_style.bg_color = Color(0.2, 0.2, 0.2, 0.8)
-	focus_style.border_width_left = 2
-	focus_style.border_width_top = 2
-	focus_style.border_width_right = 2
-	focus_style.border_width_bottom = 2
-	focus_style.border_color = Color(1, 1, 1, 0.8)
-	
-	pause_screen.get_node("VBoxContainer/ResumeButton").add_theme_stylebox_override("focus", focus_style)
-	pause_screen.get_node("VBoxContainer/BackToMenuButton").add_theme_stylebox_override("focus", focus_style)
-	
-	# Set initial focus
-	pause_screen.get_node("VBoxContainer/ResumeButton").grab_focus()
-
 func _input(event):
 	if event.is_action_pressed("pause"):
 		if pause_screen.visible:
@@ -38,3 +23,36 @@ func _input(event):
 			# If pause screen is not visible, pause the game
 			pause_screen.visible = true
 			get_tree().paused = true
+			# Set up focus style and grab focus when pause screen becomes visible
+			setup_pause_screen_focus()
+
+func setup_pause_screen_focus():
+	# Create focus style
+	var focus_style = StyleBoxFlat.new()
+	focus_style.bg_color = Color(0.2, 0.2, 0.2, 0.8)
+	focus_style.border_width_left = 2
+	focus_style.border_width_top = 2
+	focus_style.border_width_right = 2
+	focus_style.border_width_bottom = 2
+	focus_style.border_color = Color(1, 1, 1, 0.8)
+	
+	# Get buttons
+	var resume_button = pause_screen.get_node("VBoxContainer/ResumeButton")
+	var back_to_menu_button = pause_screen.get_node("VBoxContainer/BackToMenuButton")
+	
+	# Set up focus mode
+	resume_button.focus_mode = Control.FOCUS_ALL
+	back_to_menu_button.focus_mode = Control.FOCUS_ALL
+	
+	# Apply focus style
+	resume_button.add_theme_stylebox_override("focus", focus_style)
+	back_to_menu_button.add_theme_stylebox_override("focus", focus_style)
+	
+	# Set up focus navigation
+	resume_button.focus_neighbor_top = back_to_menu_button.get_path()
+	resume_button.focus_neighbor_bottom = back_to_menu_button.get_path()
+	back_to_menu_button.focus_neighbor_top = resume_button.get_path()
+	back_to_menu_button.focus_neighbor_bottom = resume_button.get_path()
+	
+	# Set initial focus
+	resume_button.grab_focus()
